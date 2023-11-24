dd000e3a3dee571d16b78f65fda76df63858d04a
Roger Wells <rsw359@gmail.com>
2023-11-24 13:12:28 +0900

    created registration form with validation

--- doeslist/views.py
+++ doeslist/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.views import LoginView, LogoutView,
+from django.http import HttpResponse
+from.models import Job, Custom_User
+
 
 # Create your views here.
